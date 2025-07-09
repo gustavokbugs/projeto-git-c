55f9cea9f5adad6420f1cb4f24ebc4ec72b714db
Gustavo Bugs <gustavo.bugs@mksolutions.com.br>
2025-07-09 17:08:25 -0300

    Desenvolvendo resposta do exercício

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int numeros[5];
-    
-    // TODO: Preencha o vetor com 5 números inteiros lidos do teclado
-    // Enunciado: Peça ao usuário para digitar 5 números inteiros e armazene no vetor 'numeros'
+    int numeros[5];  // Declara um vetor de 5 inteiros
 
+    // Lê 5 números inteiros do usuário e armazena no vetor
+    // Usamos um laço for para evitar escrever scanf várias vezes
     for (int i = 0; i < 5; i++) {
-        // exemplo de leitura: scanf("%d", &numeros[i]);
+        printf("Digite o %dº número: ", i + 1);
+        scanf("%d", &numeros[i]);  // Armazena cada número na posição correspondente do vetor
     }
 
     // Exibe os números digitados
+    // Aqui usamos outro laço for para percorrer o vetor e imprimir cada valor
     printf("Você digitou: ");
     for (int i = 0; i < 5; i++) {
-        printf("%d ", numeros[i]);
+        printf("%d ", numeros[i]);  // Mostra o valor armazenado em cada posição do vetor
     }
     printf("\n");
 
-    return 0;
+    return 0;  // Indica que o programa terminou corretamente
 }
